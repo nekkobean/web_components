ca564dd5580753d7dc870155ed1b65169a055c29
nekkobean <llenaeloisa@gmail.com>
2026-05-05 16:51:38 +0800

    feat[Input, Checkbox]: creation of text field and checkbox

---
+++ src/component/input/TextField.tsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+
+interface TextFieldProps {
+    id: string;
+    labelText: string;
+    placeholder: string;
+    error?: boolean;
+    helperText?: string;
+    type: "text" | "email" | "password" | "number";
+
+}
+
+const errorColor = "border-red-500";
+const errorTextColor = "text-red-500";
+const defaultColor = "border-gray-300";
+export const TextField: React.FC<TextFieldProps> = ({ 
+    id, labelText, placeholder, type, error, helperText
+ }) => {
+    return (
+        <div className="flex flex-col gap-1">
+            <label htmlFor={id} className="text-sm font-medium text-gray-700">
+                {labelText}
+            </label>
+            <input
+                id={id}
+                placeholder={placeholder}
+                type={type}
+                className={`border ${error ? errorColor : defaultColor} rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+            />  
+            {helperText && <p className={`${error ? errorTextColor : 'text-gray-500'} text-xs`}>{helperText}</p>}
+        </div>
+
+    );
+};
